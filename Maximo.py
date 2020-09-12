2fea6947f0b8bc9171d161ad1cf00f3c528b8f04
alexis <alexis.garcia170698@gmail.com>
2020-09-12 12:05:50 -0600

    Finally

---
+++ Maximo.py
@@ -0,0 +1,15 @@
+from math import floor
+
+
+def mcd(x, y):
+    if x < y:
+        return mcd(y, x)
+
+    while y != 0:
+        x, y = y, x % y
+
+    return x
+
+
+print(mcd(25, 2))
+print(mcd(99, 88))
